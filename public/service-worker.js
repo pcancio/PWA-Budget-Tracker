cf1e4911f2971bf1a7835d10dd0b4eb37a7afff2
pcancio <pcancio@gmail.com>
2022-02-20 15:59:44 -0500

    linked manifest, service-worker, idb

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,20 @@
-const CACHE_NAME = 'my-site-cache-v1';
-const DATA_CACHE_NAME = 'data-cache-v1';
+const CACHE_NAME = 'budget-cache-v1';
+const DATA_CACHE_NAME = 'budget-cache-v1';
 
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
     '/manifest.json',
-    'public/js/idb.js',
-    'public/css',
-    'public/css/styles.css',
-    'public/icons/icon-72x72.png',
-    'public/icons/icon-96x96.png',
-    'public/icons/icon-128x128.png',
-    'public/icons/icon-144x144.png',
-    'public/icons/icon-152x152.png',
-    'public/icons/icon-192x192.png',
-    'public/icons/icon-384x384.png',
-    'public/icons/icon-512x512.png'
+    '/js/idb.js',
+    '/css/styles.css',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png'
 ];
 
 // Install the service worker
